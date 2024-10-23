59c7bdc474a3fbd554633984d9287ba425e607ce
qw225967 <396139919@qq.com>
2024-10-23 13:46:26 +0800

    [chored] add code

--- cmake-build-debug/deps/libuv/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/frankie/code/github/Bifrost-SDK/deps/libuv
-# Build directory: /Users/frankie/code/github/Bifrost-SDK/cmake-build-debug/deps/libuv
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
